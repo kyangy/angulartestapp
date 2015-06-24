ecbf6f41399b42d983a1e8ced6a594ccfa0635d5
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-06-24 15:07:40 -0700

    started angular rails app

---
+++ app/assets/javascripts/controllers/maincontroller.js
@@ -0,0 +1,11 @@
+(function(){
+	
+var testAppControllers = angular.module('testAppControllers',[]);
+
+testAppControllers.controller('testAppListCtrl', ['$scope', '$http'
+	function($scope, $http){
+		$scope.
+}]);
+
+
+})();
